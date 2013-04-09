883d434a4d7c8d2820834606067ac7f8b6275ff7
Andrew Davis <andrew@andrewrdavis.net>
2013-04-08 22:01:23 -0400

    Add google_analytics.js to be precompiled

--- config/application.rb
+++ config/application.rb
@@ -75,7 +75,7 @@ module RubyRevealed
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['prettyify.js']
+    config.assets.precompile += ['prettyify.js', 'google_analytics.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
